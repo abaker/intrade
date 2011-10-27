10b0f43128779e40c3851e2ff4c1cc64423478cf
Alex Baker <baker.alex@gmail.com>
2011-10-27 11:05:57 -0500

    implemented data api

--- src/main/scala/org/intrade/data/API.scala
+++ src/main/scala/org/intrade/data/API.scala
@@ -1,11 +1,48 @@
 package org.intrade.data
 
-import xml.Node
+import io.Source.fromInputStream
+import org.intrade.Environment._
+import org.intrade.data.Response._
+import java.net.URL
+import xml.{XML, Node}
+
+object API {
+  def apply(env: Environment) = new API {
+    private val urls = new URLProvider(env)
+
+    def activeContractListing(optionalEventClass: Int = 0) =
+      process(urls.activeContractListing(optionalEventClass), xmlRequest, node2EventClassResponse)
+
+    def priceInformation(contractIds: Seq[String], timestamp: Long = 0, depth: Int = 5) =
+      process(urls.priceInformation(contractIds, timestamp, depth), xmlRequest, node2PriceInformationResponse)
+
+    def contractInformation(contractIds: Seq[String]) =
+      process(urls.contractInformation(contractIds), xmlRequest, node2ContractInformationResponse)
+
+    def closingPrices(contractId: String) =
+      process(urls.closingPrices(contractId), xmlRequest, node2ClosingPriceResponse)
+
+    def dailyTimeAndSales(contractId: String) =
+      process(urls.dailyTimeAndSales(contractId), stringRequest, string2TradeResponse)
+  }
+
+  private def process[A, B](request: String, getResponse: String => A, toResult: (String, A) => Response[A, B]) = {
+    toResult(request, getResponse(request))
+  }
+
+  private def stringRequest(url: String) = {
+    val stream = new URL(url).openStream
+    val source = fromInputStream(stream)
+    source.getLines().mkString
+  }
+
+  private def xmlRequest(url: String) = XML.loadString(stringRequest(url))
+}
 
 trait API {
   def activeContractListing(optionalEventClass: Int = 0): Response[Node, EventClass]
 
-  def priceInformation(contractIds: Seq[String], timestamp: Long = 0, depth: Long = 0): Response[Node, PriceInformation]
+  def priceInformation(contractIds: Seq[String], timestamp: Long = 0, depth: Int = 0): Response[Node, PriceInformation]
 
   def contractInformation(contractIds: Seq[String]): Response[Node, ContractInformation]
 
