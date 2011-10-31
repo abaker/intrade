091a331885a69c9b2bf4c9c272ce0f791247b8ef
Alex Baker <baker.alex@gmail.com>
2011-10-31 08:52:22 -0500

    removed explicit return type

--- src/main/scala/org/intrade/ContractState.scala
+++ src/main/scala/org/intrade/ContractState.scala
@@ -1,12 +1,12 @@
 package org.intrade
 
-import xml.{NodeSeq, Node}
+import xml.NodeSeq
 
 object ContractState extends Enumeration {
   type ContractState = Value
   val Initialized, Open, Paused, Session_Closed, Closed_for_Expiry, Settled, Canceled, Reversed = Value
 
-  def parse(state: String): ContractState = state match {
+  def parse(state: String) = state match {
     case "I" => ContractState.Initialized
     case "O" => ContractState.Open
     case "P" => ContractState.Paused
@@ -15,10 +15,7 @@ object ContractState extends Enumeration {
     case "S" => ContractState.Settled
     case "X" => ContractState.Canceled
     case "R" => ContractState.Reversed
-    case x => throw new RuntimeException("Unknown Contract State: %s" format (x))
   }
 
-  implicit def nodeSeq2ContractState(node: NodeSeq): ContractState = parse(node.text)
-
-  implicit def attribute2ContractState(node: Option[Seq[Node]]): ContractState = parse(node.get.text)
+  implicit def nodeSeq2ContractState(node: NodeSeq) = parse(node.text)
 }
