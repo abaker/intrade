7a0758d3d08738f1adbac85cfc13b7d01432842a
Alex Baker <baker.alex@gmail.com>
2011-10-31 18:40:43 -0500

    added multi order tests

--- src/test/scala/org/intrade/trading/RequestsTest.scala
+++ src/test/scala/org/intrade/trading/RequestsTest.scala
@@ -1,11 +1,25 @@
 package org.intrade.trading
 
+import org.intrade.trading.Side._
+import org.intrade.trading.TimeInForce._
+import org.intrade.trading.OrderType._
 import org.intrade.trading.Requests._
 import org.scalatest.FunSuite
 import xml.Utility.trim
 import xml.Node
 
 class RequestsTest extends FunSuite {
+
+  case class OrderRequestImpl(conID: Int,
+                              side: Side,
+                              quantity: Int,
+                              limitprice: BigDecimal,
+                              override val userReference: String = "",
+                              override val timeInForce: TimeInForce = Good_Til_Cancel,
+                              override val timeToExpire: Long = 0L,
+                              override val orderType: OrderType = Limit,
+                              override val touchPrice: BigDecimal = 0)
+    extends OrderRequest
 
   test("create login request") {
     val expected =
@@ -165,20 +179,74 @@ class RequestsTest extends FunSuite {
     compareXml(expected, cancelMultipleOrdersForUser(List(1234, 5678)))
   }
 
-  test("create basic limit order request") {
+  test("create basic limit orders") {
     val expected =
       <xmlrequest requestOp="multiOrderRequest">
         <order>conID=1234,side=B,quantity=3,limitPrice=45.5,timeInForce=GTC</order>
+        <order>conID=5678,side=S,quantity=25,limitPrice=50.0,timeInForce=GTC</order>
+        <order>conID=999,side=B,quantity=1,limitPrice=0.1,timeInForce=GTC,userReference=abc123</order>
       </xmlrequest>
 
-    val orderRequest = new OrderRequest {
-      val conID = 1234
-      val side = Side.Buy
-      val quantity = 3
-      val limitprice = BigDecimal(45.5)
-    }
+    val buy = OrderRequestImpl(1234, Buy, 3, 45.5)
+    val sell = OrderRequestImpl(5678, Sell, 25, 50.0)
+    val userRef = OrderRequestImpl(999, Buy, 1, 0.1, "abc123")
 
-    compareXml(expected, multiOrderRequest(List(orderRequest)))
+    compareXml(expected, multiOrderRequest(List(buy, sell, userRef)))
+  }
+
+  test("gfs and gtt") {
+    val expected =
+      <xmlrequest requestOp="multiOrderRequest">
+        <order>conID=1234,side=B,quantity=1,limitPrice=99.9,timeInForce=GFS</order>
+        <order>conID=1234,side=B,quantity=1,limitPrice=99.9,timeInForce=GFS,userReference=aaa111</order>
+        <order>conID=5678,side=S,quantity=1,limitPrice=11.1,timeInForce=GTT,timeToExpire=12345</order>
+        <order>conID=9191,side=S,quantity=100,limitPrice=50,timeInForce=GTT,timeToExpire=98765,userReference=abc123</order>
+      </xmlrequest>
+
+    val gfs = OrderRequestImpl(1234, Buy, 1, 99.9, "", Good_For_Session)
+    val gfsWithUserRef = OrderRequestImpl(1234, Buy, 1, 99.9, "aaa111", Good_For_Session)
+    val gtt = OrderRequestImpl(5678, Sell, 1, 11.1, "", Good_Til_Time, 12345L)
+    val gttWithUserRef = OrderRequestImpl(9191, Sell, 100, 50, "abc123", Good_Til_Time, 98765L)
+
+    compareXml(expected, multiOrderRequest(List(gfs, gfsWithUserRef, gtt, gttWithUserRef)))
+  }
+
+  test("fok and touch orders") {
+    val expected =
+      <xmlrequest requestOp="multiOrderRequest">
+        <order>conID=1234,side=B,quantity=1,limitPrice=99.9,timeInForce=GTC,orderType=F</order>
+        <order>conID=1234,side=S,quantity=1,limitPrice=99.9,timeInForce=GTC,orderType=F,userReference=aaa111</order>
+        <order>conID=5678,side=S,quantity=1,limitPrice=11.1,timeInForce=GTC,orderType=T,touchPrice=45.0</order>
+        <order>conID=9191,side=B,quantity=1,limitPrice=50,timeInForce=GTC,orderType=T,touchPrice=99,userReference=abc123</order>
+      </xmlrequest>
+
+    val fok = OrderRequestImpl(1234, Buy, 1, 99.9, "", Good_Til_Cancel, 0, OrderType.Fill_Or_Kill)
+    val fokWithUserRef = OrderRequestImpl(1234, Sell, 1, 99.9, "aaa111", Good_Til_Cancel, 0, OrderType.Fill_Or_Kill)
+    val touch = OrderRequestImpl(5678, Sell, 1, 11.1, "", Good_Til_Cancel, 0, OrderType.Touch, 45.0)
+    val touchWithUserRef = OrderRequestImpl(9191, Buy, 1, 50, "abc123", Good_Til_Cancel, 0, OrderType.Touch, 99)
+
+    compareXml(expected, multiOrderRequest(List(fok, fokWithUserRef, touch, touchWithUserRef)))
+  }
+
+  test("multi order cancel previous orders") {
+    val expected =
+      <xmlrequest requestOp="multiOrderRequest">
+        <cancelPrevious>true</cancelPrevious>
+        <order>conID=1234,side=B,quantity=3,limitPrice=45.5,timeInForce=GTC</order>
+      </xmlrequest>
+
+    compareXml(expected, multiOrderRequest(List(OrderRequestImpl(1234, Buy, 3, 45.5)), true))
+  }
+
+  test("multi order cancel with quick cancel") {
+    val expected =
+      <xmlrequest requestOp="multiOrderRequest">
+        <cancelPrevious>true</cancelPrevious>
+        <quickCancel>true</quickCancel>
+        <order>conID=1234,side=B,quantity=3,limitPrice=45.5,timeInForce=GTC</order>
+      </xmlrequest>
+
+    compareXml(expected, multiOrderRequest(List(OrderRequestImpl(1234, Buy, 3, 45.5)), true, true))
   }
 
   private def compareXml(expected: Node, actual: Node) {
