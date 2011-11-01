d29aeb4e633b3f083a08b094902f192b9179804a
Alex Baker <baker.alex@gmail.com>
2011-11-01 15:56:27 -0500

    added stop

--- src/main/scala/org/intrade/trading/OrderType.scala
+++ src/main/scala/org/intrade/trading/OrderType.scala
@@ -4,11 +4,12 @@ import xml.NodeSeq
 
 object OrderType extends Enumeration {
   type OrderType = Value
-  val Limit, Touch, Fill_Or_Kill = Value
+  val Limit, Touch, Fill_Or_Kill, Stop = Value
 
   implicit def nodeSeq2OrderType(node: NodeSeq) = node.text match {
     case "Limit" => Limit
     case "Touch" => Touch
     case "FillorKill" => Fill_Or_Kill
+    case "Stop" => Stop
   }
 }
