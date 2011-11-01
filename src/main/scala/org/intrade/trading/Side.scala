7bfcf13dccff7b226b94acd9586894d2e8a6a435
Alex Baker <baker.alex@gmail.com>
2011-11-01 15:07:34 -0500

    updated side and tests

--- src/main/scala/org/intrade/trading/Side.scala
+++ src/main/scala/org/intrade/trading/Side.scala
@@ -6,10 +6,8 @@ object Side extends Enumeration {
   type Side = Value
   val Buy, Sell = Value
 
-  def parse(side: String) = side match {
-    case "BUY" | "B" => Side.Buy
-    case "SELL" | "S" => Side.Sell
+  implicit def nodeSeq2String(node: NodeSeq) = node.text match {
+    case "Buy" | "BUY" | "B" => Side.Buy
+    case "Sell" | "SELL" | "S" => Side.Sell
   }
-
-  implicit def nodeSeq2String(node: NodeSeq) = parse(node.text)
 }
--- src/test/scala/org/intrade/trading/SideTest.scala
+++ src/test/scala/org/intrade/trading/SideTest.scala
@@ -5,21 +5,31 @@ import org.scalatest.FunSuite
 class SideTest extends FunSuite {
   test("should parse single character sides") {
     expect(Side.Buy) {
-      Side.parse("B")
+      Side.nodeSeq2String(<node>B</node>)
     }
 
     expect(Side.Sell) {
-      Side.parse("S")
+      Side.nodeSeq2String(<node>S</node>)
+    }
+  }
+
+  test("should parse upcase sides") {
+    expect(Side.Buy) {
+      Side.nodeSeq2String(<node>BUY</node>)
+    }
+
+    expect(Side.Sell) {
+      Side.nodeSeq2String(<node>SELL</node>)
     }
   }
 
   test("should parse sides") {
     expect(Side.Buy) {
-      Side.parse("BUY")
+      Side.nodeSeq2String(<node>Buy</node>)
     }
 
     expect(Side.Sell) {
-      Side.parse("SELL")
+      Side.nodeSeq2String(<node>Sell</node>)
     }
   }
 }
