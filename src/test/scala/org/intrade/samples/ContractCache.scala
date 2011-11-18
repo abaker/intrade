ae57f217d870b2a7ef64ab695721d6e9dbac2e41
Alex Baker <baker.alex@gmail.com>
2011-11-18 16:10:39 -0600

    added sample contract cache

---
+++ src/test/scala/org/intrade/samples/ContractCache.scala
@@ -0,0 +1,67 @@
+package org.intrade.samples
+
+import org.intrade._
+import ContractCache._
+import data.Response
+import java.util.concurrent.TimeUnit
+import java.io.{FileWriter, File}
+import java.util.Date
+
+/*
+From the Intrade API Documentation:
+"As [the contract listing] is a large file, [it] should only be retrieved at start-up and not more than one time per 15 minutes."
+
+The purpose of this class is to store contracts on disk so we may reuse them in other samples
+without violating the policy mentioned above. This is not a robust implementation as it assumes
+all file operations are successful.
+*/
+
+class ContractCache(private val api: data.API, private val filename: String = defaultCacheFile) {
+  private val file = new File(filename)
+
+  def loadEventClasses = refreshNeeded match {
+    case true => {
+      // get contract listing from intrade
+      val response = api.activeContractListing
+      // write intrade's xml response to disk
+      writeToFile(response.raw)
+      // return response
+      response
+    }
+    case false => {
+      // fetch intrade's xml response from disk
+      val xmlString = readFromFile
+      // convert to response and return
+      Response.node2EventClassResponse(file.getAbsolutePath, xmlString)
+    }
+  }
+
+  private def refreshNeeded = cacheDoesNotExist || cacheIsStale
+
+  private def cacheDoesNotExist = !file.exists()
+
+  private def cacheIsStale = file.lastModified() < new Date().getTime - TimeUnit.MINUTES.toMillis(15)
+
+  private def writeToFile(contents: String) {
+    using(new FileWriter(filename, false)) {
+      _.write(contents)
+    }
+  }
+
+  private def readFromFile = {
+    using(scala.io.Source.fromFile(filename)) {
+      _.mkString
+    }
+  }
+}
+
+object ContractCache {
+  val defaultCacheFile = "./out/intrade_contract_cache.xml"
+
+  private def using[A <: {def close()}, B](resource: A)(f: A => B) =
+    try {
+      f(resource)
+    } finally {
+      resource.close()
+    }
+}
